1a3add47b5cfa52e03c3df8c1e5ed38aaf17ee4e
lightnoway <lightnoway@qq.com>
2023-12-02 10:34:45 +0800

    刷题 12_02

--- playground/medium/00110-medium-capitalize.ts
+++ playground/medium/00110-medium-capitalize.ts
@@ -18,43 +18,96 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyCapitalize<S extends string> = any
+type MapCapital = {
+  a: "A";
+  b: "B";
+  c: "C";
+  d: "D";
+  e: "E";
+  f: "F";
+  g: "G";
+  h: "H";
+  i: "I";
+  j: "J";
+  k: "K";
+  l: "L";
+  m: "M";
+  n: "N";
+  o: "O";
+  p: "P";
+  q: "Q";
+  r: "R";
+  s: "S";
+  t: "T";
+  u: "U";
+  v: "V";
+  w: "W";
+  x: "X";
+  y: "Y";
+  z: "Z";
+};
+type DiyToUpper<T> = T extends keyof MapCapital ? MapCapital[T]:T
+type LogCase = DiyToUpper<'a'>
+
+
+// try 1
+// type MyCapitalize<S extends string> =
+// // S[0]  //d:问题：S[0] 是string 太宽松了; 
+//   S extends `${S[0]}${infer Rest}`
+//     ? S[0] extends keyof MapCapital?`${MapCapital[S[0]]}${Rest}`:S
+//     : S;
+
+// 解2    
+type MyCapitalize<S extends string> =
+// S[0]  //问题：S[0] 是string 太宽松了
+  S extends `${infer First}${infer Rest}`
+    ? `${DiyToUpper<First>}${Rest}`
+    : S;
+
+/* 
+ S[0] 是string 太宽松：试其他办法
+ - 取首字母的办法还有infer；
+  - 多 infer 不加限定时`${infer First}${infer Rest}` 第一个取1个字母 非贪婪，后边取剩下
+  - 多 infer 加限定时`${infer I1} - ${infer I 2}` 根据限定来
+*/
+
+type Log =    MyCapitalize<"foobar"> 
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<MyCapitalize<'foobar'>, 'Foobar'>>,
-  Expect<Equal<MyCapitalize<'FOOBAR'>, 'FOOBAR'>>,
-  Expect<Equal<MyCapitalize<'foo bar'>, 'Foo bar'>>,
-  Expect<Equal<MyCapitalize<''>, ''>>,
-  Expect<Equal<MyCapitalize<'a'>, 'A'>>,
-  Expect<Equal<MyCapitalize<'b'>, 'B'>>,
-  Expect<Equal<MyCapitalize<'c'>, 'C'>>,
-  Expect<Equal<MyCapitalize<'d'>, 'D'>>,
-  Expect<Equal<MyCapitalize<'e'>, 'E'>>,
-  Expect<Equal<MyCapitalize<'f'>, 'F'>>,
-  Expect<Equal<MyCapitalize<'g'>, 'G'>>,
-  Expect<Equal<MyCapitalize<'h'>, 'H'>>,
-  Expect<Equal<MyCapitalize<'i'>, 'I'>>,
-  Expect<Equal<MyCapitalize<'j'>, 'J'>>,
-  Expect<Equal<MyCapitalize<'k'>, 'K'>>,
-  Expect<Equal<MyCapitalize<'l'>, 'L'>>,
-  Expect<Equal<MyCapitalize<'m'>, 'M'>>,
-  Expect<Equal<MyCapitalize<'n'>, 'N'>>,
-  Expect<Equal<MyCapitalize<'o'>, 'O'>>,
-  Expect<Equal<MyCapitalize<'p'>, 'P'>>,
-  Expect<Equal<MyCapitalize<'q'>, 'Q'>>,
-  Expect<Equal<MyCapitalize<'r'>, 'R'>>,
-  Expect<Equal<MyCapitalize<'s'>, 'S'>>,
-  Expect<Equal<MyCapitalize<'t'>, 'T'>>,
-  Expect<Equal<MyCapitalize<'u'>, 'U'>>,
-  Expect<Equal<MyCapitalize<'v'>, 'V'>>,
-  Expect<Equal<MyCapitalize<'w'>, 'W'>>,
-  Expect<Equal<MyCapitalize<'x'>, 'X'>>,
-  Expect<Equal<MyCapitalize<'y'>, 'Y'>>,
-  Expect<Equal<MyCapitalize<'z'>, 'Z'>>,
-]
+  Expect<Equal<MyCapitalize<"foobar">, "Foobar">>,
+  Expect<Equal<MyCapitalize<"FOOBAR">, "FOOBAR">>,
+  Expect<Equal<MyCapitalize<"foo bar">, "Foo bar">>,
+  Expect<Equal<MyCapitalize<"">, "">>,
+  Expect<Equal<MyCapitalize<"a">, "A">>,
+  Expect<Equal<MyCapitalize<"b">, "B">>,
+  Expect<Equal<MyCapitalize<"c">, "C">>,
+  Expect<Equal<MyCapitalize<"d">, "D">>,
+  Expect<Equal<MyCapitalize<"e">, "E">>,
+  Expect<Equal<MyCapitalize<"f">, "F">>,
+  Expect<Equal<MyCapitalize<"g">, "G">>,
+  Expect<Equal<MyCapitalize<"h">, "H">>,
+  Expect<Equal<MyCapitalize<"i">, "I">>,
+  Expect<Equal<MyCapitalize<"j">, "J">>,
+  Expect<Equal<MyCapitalize<"k">, "K">>,
+  Expect<Equal<MyCapitalize<"l">, "L">>,
+  Expect<Equal<MyCapitalize<"m">, "M">>,
+  Expect<Equal<MyCapitalize<"n">, "N">>,
+  Expect<Equal<MyCapitalize<"o">, "O">>,
+  Expect<Equal<MyCapitalize<"p">, "P">>,
+  Expect<Equal<MyCapitalize<"q">, "Q">>,
+  Expect<Equal<MyCapitalize<"r">, "R">>,
+  Expect<Equal<MyCapitalize<"s">, "S">>,
+  Expect<Equal<MyCapitalize<"t">, "T">>,
+  Expect<Equal<MyCapitalize<"u">, "U">>,
+  Expect<Equal<MyCapitalize<"v">, "V">>,
+  Expect<Equal<MyCapitalize<"w">, "W">>,
+  Expect<Equal<MyCapitalize<"x">, "X">>,
+  Expect<Equal<MyCapitalize<"y">, "Y">>,
+  Expect<Equal<MyCapitalize<"z">, "Z">>
+];
 
 /* _____________ Further Steps _____________ */
 /*
--- playground/medium/00119-medium-replaceall.ts
+++ playground/medium/00119-medium-replaceall.ts
@@ -18,22 +18,70 @@
 
 /* _____________ Your Code Here _____________ */
 
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer Before}${From}${infer After}`
+  ? `${Before}${To}${After}`
+  : S;
+
+//d:?:怎么额外弄个变量
+//
+type extraT<S extends string, From extends string, To extends string> = Replace<
+  S,
+  From,
+  To
+> extends infer Result
+  ? Result
+  : 1;
+type Log = extraT<"foobar", "bar", "foo">;
+
+// -try1: 递归边缘： replace过的 不继续replace :ReplaceAll<"foobarfoobar", "ob", "b">
+// type ReplaceAll<
+//   S extends string,
+//   From extends string,
+//   To extends string
+// > = Replace<S, From, To> extends infer Result
+//   ? Result extends S
+//     ? Result
+//     : Result extends string
+//     ? ReplaceAll<Result, From, To>
+//     : 1
+//   : S;
+
+// 解2：
+type ReplaceAll<
+  S extends string,
+  From extends string,
+  To extends string
+> =
+ From extends ""|To
+  ? S
+  : S extends `${infer L}${From}${infer R}`
+  ? `${L}${To}${ReplaceAll<R, From, To>}`
+  : S;
+
+// type Log2 = ReplaceAll<"foobarfoobar", "ob", "b">;
+type Log2 = ReplaceAll<"foobarbar", "", "foo">;
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
-  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
-  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
-  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
-  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
-  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
-  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
-  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
-  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
-]
+  Expect<Equal<ReplaceAll<"foobar", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobar", "bag", "foo">, "foobar">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "bar", "foo">, "foofoofoo">>,
+  Expect<Equal<ReplaceAll<"t y p e s", " ", "">, "types">>,
+  Expect<Equal<ReplaceAll<"foobarbar", "", "foo">, "foobarbar">>,
+  Expect<Equal<ReplaceAll<"barfoo", "bar", "foo">, "foofoo">>,
+  Expect<Equal<ReplaceAll<"foobarfoobar", "ob", "b">, "fobarfobar">>, //递归边缘： replace过的 不继续replace
+  Expect<Equal<ReplaceAll<"foboorfoboar", "bo", "b">, "foborfobar">>,
+  Expect<Equal<ReplaceAll<"", "", "">, "">>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'bar'>, 'barfoo'>>//排除不必要的执行
+];
 
 /* _____________ Further Steps _____________ */
 /*
